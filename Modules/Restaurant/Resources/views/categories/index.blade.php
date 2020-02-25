b4af1926b87002a5eba5996cd613bd82b80096cf
Wilson <jwrdc82@gmail.com>
2020-02-25 22:25:36 +0000

    fix translations

--- Modules/Restaurant/Resources/views/categories/index.blade.php
+++ Modules/Restaurant/Resources/views/categories/index.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts.app', ['title' => __('Categories Management')])
+@extends('layouts.app', ['title' => __('restaurant::lang.categories_management')])
 
 @section('content')
     @include('layouts.headers.cards')
 
     @component('components.tables.table', ['rows' => $categories, 'columnsValues' => ['id', 'name'], 'baseRoute' => 'restaurant.category'])
-        @slot('title'){{ __('Categories') }}@endslot
+        @slot('title'){{ __('estaurant::lang.categories') }}@endslot
 
         @slot('createRoute')
             <a href="{{ route('restaurant.category.create') }}"
-               class="btn btn-sm btn-primary">{{ __('Add category') }}</a>
+               class="btn btn-sm btn-primary">{{ __('estaurant::lang.add_category') }}</a>
         @endslot
     @endcomponent
 @endsection
